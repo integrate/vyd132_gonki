4db142c81beb9db05469cb929e710daa6faffb09
USER <hello@gsdadg.asdsadasd>
2024-01-03 19:08:09 +0300

    hi

--- plane.py
+++ plane.py
@@ -15,12 +15,16 @@ points = 0
 expl_time = 0
 reload_speed = 3
 s_level = 0
-wawe = 1
-gruz_enemies = 5
-enemy_hit = 0
-istrebitel_enemies = 10
+wawe = 0
+gruz_enemies = 0
+istrebitel_enemies = 0
 big_plane_enemies = 0
 time_wawe =0
+gruz_enemie_hit=0
+istrebitel_enemie_hit=0
+big_plane_enemie_hit=0
+time_wawe= 0
+
 
 wrap.world.create_world(width,height)
 bullet = None
@@ -29,8 +33,6 @@ expl = None
 sky = wrap.sprite.add("back",width/2,height/2-220,"sky")
 ground = wrap.sprite.add("back",width/2,height/2+220,"images")
 turrel = wrap.sprite.add("turrel",width/2,height/2+190,"turrel")
-plane = wrap.sprite.add("cars", -500, random.randint(50, 300), "plane")
-istribitel = wrap.sprite.add("cars", -500, random.randint(50, 300), "istribitel")
 coin = wrap.sprite.add("mario-items",width-60,24,"coin")
 heart = wrap.sprite.add("back",width-130,80,"heart")
 speed_button = wrap.sprite.add("back",width-40,height-70,"speed")
@@ -39,7 +41,6 @@ wrench = wrap.sprite.add("back",60,height-70,"wrench")
 plane_wawe = wrap.sprite.add("cars", 50, 40, "plane")
 istribitel_wawe = wrap.sprite.add("cars", 50, 60, "istribitel")
 bullet_level = wrap.sprite.add("turrel", width -195, height -80, "patron")
-big_plane = wrap.sprite.add("cars", -500, random.randint(50, 300), "big_plane")
 big_plane_wawe = wrap.sprite.add("cars", 50, 80, "big_plane")
 
 point = wrap.sprite.add_text(str(points),width-30,24,font_size=60,text_color=[255,160,68])
@@ -62,10 +63,7 @@ wrap.sprite.set_angle(bullet_level,30)
 wrap.sprite.set_size(mouse,1,1)
 
 wrap.sprite.set_size_percent(speed_button,10,10)
-wrap.sprite.set_size_percent(plane,50,50)
 wrap.sprite.set_size_percent(plane_wawe,10,10)
-wrap.sprite.set_size_percent(istribitel,5,5)
-wrap.sprite.set_size_percent(big_plane,10,10)
 wrap.sprite.set_size_percent(istribitel_wawe,2,2)
 wrap.sprite.set_size_percent(big_plane_wawe,5,5)
 wrap.sprite.set_size_percent(wrench,10,10)
@@ -75,10 +73,13 @@ wrap.sprite.set_size_percent(coin,150,150)
 wrap.sprite.set_size_percent(heart,15,15)
 wrap.sprite.set_size_percent(ground,300,100)
 
-wrap.sprite.set_reverse_x(istribitel,True)
+
 wrap.sprite.set_reverse_x(istribitel_wawe,True)
-wrap.sprite.set_reverse_x(big_plane,True)
 wrap.sprite.set_reverse_x(big_plane_wawe,True)
+
+plane = None
+istribitel=None
+big_plane=None
 
 @wrap.on_mouse_move()
 def turrel_move(pos_x,pos_y):
@@ -156,7 +157,7 @@ def bullet_speed_button():
     if click == True and points >= 6:
         bullet_speed_level += 1
         wrap.sprite_text.set_text(bullet_text, str(bullet_speed_level))
-        bullet_speed_level += 0.5
+        bullet_speed += 0.5
         points_check(-6)
 
 
@@ -172,6 +173,7 @@ def wawes():
     if big_plane_enemies == 0:
         wrap.sprite.remove(big_plane)
         big_plane=None
+    wawe_perexod(0, 0, 10, 0)
     wawe_perexod(1,5,10,0)
     wawe_perexod(2, 10, 10, 0)
     wawe_perexod(3,10,10,5)
@@ -190,24 +192,37 @@ def wawe_check(col):
     wrap.sprite_text.set_text(wawe_text,"Волна номер: " + str(wawe))
 
 def wawe_perexod(old_wawe_number,gruz_enemies_col,istrebitel_enemies_col,big_plane_enemies_col):
-    global start,plane,istribitel,big_plane
-    if big_plane_enemies == 0 and gruz_enemies == 0 and istrebitel_enemies == 0 and wawe == old_wawe_number:
-        wawe_check(1)
-        def_time = time.time()
-        timer = def_time - start
-        if timer >= 1:
-            time_wawe += 1
-            start = time.time()
-            if time_wawe == 5:
-                if gruz_enemies_col >0:
-                    plane = wrap.sprite.add("cars", -500, random.randint(50, 300), "plane")
-                if istrebitel_enemies_col>0:
-                    istribitel = wrap.sprite.add("cars", -500, random.randint(50, 300), "istribitel")
-                if big_plane_enemies_col>0:
-                    big_plane = wrap.sprite.add("cars", -500, random.randint(50, 300), "big_plane")
-                gruz_check(gruz_enemies_col)
-                istribitel_check(istrebitel_enemies_col)
-                big_plane_check(big_plane_enemies_col)
+    global start,plane,istribitel,big_plane,time_wawe
+    if wawe != old_wawe_number:
+        return
+    if gruz_enemies>0 or istrebitel_enemies>0 or big_plane_enemies>0:
+        return
+
+    def_time = time.time()
+    timer = def_time - start
+    print(time_wawe)
+    if timer < 1:
+        return
+    time_wawe += 1
+    start = time.time()
+    if time_wawe != 5:
+        return
+    if gruz_enemies_col >0:
+        plane = wrap.sprite.add("cars", -500, random.randint(50, 300), "plane")
+        wrap.sprite.set_size_percent(plane,50,50)
+    if istrebitel_enemies_col>0:
+        istribitel = wrap.sprite.add("cars", -500, random.randint(50, 300), "istribitel")
+        wrap.sprite.set_size_percent(istribitel, 5, 5)
+        wrap.sprite.set_reverse_x(istribitel,True)
+    if big_plane_enemies_col>0:
+        big_plane = wrap.sprite.add("cars", -500, random.randint(50, 300), "big_plane")
+        wrap.sprite.set_size_percent(big_plane, 10, 10)
+        wrap.sprite.set_reverse_x(big_plane, True)
+    wawe_check(1)
+    time_wawe=0
+    gruz_check(gruz_enemies_col)
+    istribitel_check(istrebitel_enemies_col)
+    big_plane_check(big_plane_enemies_col)
 
 
 def lives_check(col):
@@ -245,7 +260,29 @@ def hp_check():
         wrap.sprite_text.set_text_color(heal, 121,240,47)
 
 def plane_home(sprite_id):
-    wrap.sprite.move_to(sprite_id,-100,random.randint(50, 300))
+    wrap.sprite.move_to(sprite_id,-300,random.randint(50, 300))
+
+def plane_hit(id,hp,enemie,points_give,enemie_hit):
+    global points,point,bullet,plane,gruz_enemies,istrebitel_enemies,big_plane_enemies
+    if bullet == None:
+        return enemie_hit
+    if id==None:
+        return enemie_hit
+    shot = wrap.sprite.is_collide_sprite(bullet, id)
+    if shot != True:
+        return enemie_hit
+    enemie_hit+=1
+    wrap.sprite.remove(bullet)
+    bullet = None
+    if enemie_hit == hp:
+        plane_x = wrap.sprite.get_x(id)
+        plane_y = wrap.sprite.get_y(id)
+        boom(plane_x,plane_y)
+        plane_home(id)
+        enemie(-1)
+        points_check(points_give)
+        enemie_hit=0
+    return enemie_hit
 
 
 """
@@ -263,37 +300,23 @@ def plane_home(sprite_id):
             возвращаем самолет за экран
             вычесляем количество грузовых самолетов в волне
             начисляем очки
-тоже самое для истребителя и большого самолета
+тоже самое для истребителя и большого самолета 
 """
 
 @wrap.always(10)
-def points_get():
-    global points,point,bullet,plane,gruz_enemies,enemy_hit
-
+def bullet_move():
+    global points,point,bullet,plane,gruz_enemies,enemy_hit,gruz_enemie_hit,istrebitel_enemie_hit,big_plane_enemie_hit
     if bullet == None:
         return
-    wrap.sprite.move_at_angle_dir(bullet, bullet_speed)
-    if plane==None:
-        return
-
-    shot = wrap.sprite.is_collide_sprite(bullet,plane)
-
-    if shot == True:
-        enemy_hit +=1
-        if enemy_hit == 2:
-            plane_x = wrap.sprite.get_x(plane)
-            plane_y = wrap.sprite.get_y(plane)
-            wrap.sprite.remove(bullet)
-            bullet = None
-            boom(plane_x,plane_y)
-            plane_home(plane)
-            gruz_check(-1)
-            points_check(1)
-            enemy_hit=0
+    wrap.sprite.move_at_angle_dir(bullet,bullet_speed)
+    gruz_enemie_hit= plane_hit(plane,2,gruz_check,2,gruz_enemie_hit)
+    istrebitel_enemie_hit= plane_hit(istribitel, 1,istribitel_check, 1,istrebitel_enemie_hit)
+    big_plane_enemie_hit= plane_hit(big_plane, 3,big_plane_check, 3,big_plane_enemie_hit)
 
 
 
-@wrap.always(5)
+
+@wrap.always(10)
 def plane_move():
     global lives,plane,gruz_enemies
     if plane==None:
@@ -306,7 +329,7 @@ def plane_move():
         gruz_check(-1)
 
 
-@wrap.always(5)
+@wrap.always(10)
 def istribitel_move():
     global lives,plane,gruz_enemies,istribitel
     if istribitel==None:
@@ -315,17 +338,17 @@ def istribitel_move():
     istribitel_x = wrap.sprite.get_x(istribitel)
     if istribitel_x >=width+100:
         plane_home(istribitel)
-        lives_check(-2)
+        lives_check(-1)
         istribitel_check(-1)
 
-@wrap.always(5)
+@wrap.always(10)
 def big_plane_move():
     global lives,plane,gruz_enemies,big_plane
     if big_plane==None:
         return
-    wrap.sprite.move(big_plane, 2, 0)
-    big_plane_x = wrap.sprite.get_x(istribitel)
+    wrap.sprite.move(big_plane, 1, 0)
+    big_plane_x = wrap.sprite.get_x(big_plane)
     if big_plane_x >=width+100:
         plane_home(big_plane)
-        lives_check(-2)
+        lives_check(-3)
         big_plane_check(-1)
